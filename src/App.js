962860881870ea59b409337c876a973562187716
alexandr.livitskiy <alexandr.livitskiy@gmail.com>
2019-07-12 16:34:02 +0300

    Add react and routs

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Nav from './components/nav.jsx';
+import Home from './components/home.jsx';
+import Main from './components/main.jsx';
+import About from './components/about.jsx';
+import NotFound from './components/notfound.jsx';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+          <div>
+              <Nav />
+              <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route path="/main" component={Main} />
+                  <Route path="/about" component={About} />
+                  <Route component={NotFound} />
+              </Switch>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link, BrowserRouter}  from 'react-router-dom';
+
+export default class Nav extends React.Component{
+    render(){
+        return <div>
+            <Link to="/">Start</Link>
+            <Link to="/main">Main</Link>
+            <Link to="/about">About</Link>
+        </div>;
+    }
+}
